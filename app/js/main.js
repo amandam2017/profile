1ec554be365ae78b5fa4424aaa1e2deb699cf3ca
Amanda <amandamaarman19911@gmail.com>
2018-01-19 11:41:17 +0200

    added styles on the modal in contact.html

--- app/js/main.js
+++ app/js/main.js
@@ -46,143 +46,24 @@ function newpost() {
 //     elems[i].addEventListener('mousedown', makeActive);
 
 
+//functionality for contact button
 
+var btn = document.getElementById('mybutton');
+var modal = document.getElementById('mymodal');
+var btnc = document.getElementById('btnclose');
 
-// JS BUILD
-// print('');
-function btnplus(){
-    document.icalculator.display.value +="run1";
-    document.icalculator.display.style.textAlign="right";
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-function btnplus(){
-    document.icalculator.display.value +="+";
-    document.icalculator.display.style.textAlign="right";
+btnclose.onclick = function() {
+    mymodal.style.display = "none";
 }
 
-function btnsub(){
-    document.icalculator.display.value +="-";
-    document.icalculator.display.style.textAlign="right";
-}
+// var btn = document.getElementById('btnclose');
+// var modal = document.getElementById('form');
 
-function btnmult(){
-    document.icalculator.display.value +="*";
-    document.icalculator.display.style.textAlign="right";
-}
+// form.onclick = function() {
+//     modal.style.display = "none";
+// }
 
-function btnMod(){
-    document.icalculator.display.value +="%";
-    document.icalculator.display.style.textAlign="right";
-}
-
-function btndot(){
-    document.icalculator.display.value +=".";
-    document.icalculator.display.style.textAlign="right";
-}
-function btndiv(){
-    document.icalculator.display.value +="/";
-    document.icalculator.display.style.textAlign="right";
-}
-function btnclear(){
-    document.icalculator.display.value ="";
-    document.icalculator.display.style.textAlign="right";
-}
-
-
-// stopwatch
-
-var ss = document.getElementsByClassName('stopwatch');
-[].forEach.call(ss, function (s){
-    var currentTimer = 0,
-    interval = 0,
-    lastUpdateTime = new Date().getTime(),
-    start = s.querySelector('button.start'),
-    stop = s.querySelector('button.stop'),
-    reset = s.querySelector('button.reset'),
-    mins = s.querySelector('span.minutes'),
-    secs = s.querySelector('span.seconds'),
-    cents = s.querySelector('span.centiseconds');
-
-    
-    start.addEventListener ('click', startTimer);
-    stop.addEventListener ('click', stopTimer);
-    reset.addEventListener ('click', resetTimer);
-    
-
-    function pad (n){
-        return ('00' + n).substr(-2);
-    }
-
-    function update(){
-        var now = new Date().getTime(),
-        dt = now - lastUpdateTime;
-
-        currentTimer += dt;
-
-        var time = new Date(currentTimer);
-
-        mins.innerHTML = pad(time.getMinutes())
-        secs.innerHTML = pad(time.getSeconds())
-        cents.innerHTML = pad(Math.floor(time.getMilliseconds() /10));
-
-        lastUpdateTime = now;
-    }
-
-    function startTimer(){
-        if(!interval){
-            lastuUdateTime = new Date().getTime();
-            interval = setInterval(update, 1);
-        }
-    }
-    
-    
-
-//    function stopTimer (){
-//       clearInterval(interval);
-//       interval = 0; 
-//    }
-    
-    function stopTimer() {
-       if (interval) {
-           clearInterval(interval);
-           interval = false;
-       }
-    }
-    
-    function resetTimer() {
-       if (interval) {
-           clearInterval(interval);
-           interval = false;
-       }
-    }
-    
-
-
-//    function resetTimer (){
-//        stopTimer();
-//
-//        currentTimer = 0;
-//
-//        mins.innerHTML = secs.innerHTML = cents.innerHTML = pad(1); 
-//    }
-    
-})
-
-
-// UX KIT profile__avatar
-// var inputs = document.getElementsByClassName('indeterminate');
-
-// for(var i=0; i>inputs.length; i++);
-// inputs[i].indeterminate = true;
-
-// var button = document.getElementById('button');
-
-// button.onclick = function() {
-//     var div = document.getElementById('newpost');
-//     if (div.style.display !== 'none') {
-//         div.style.display = 'none';
-//     }
-//     else {
-//         div.style.display = 'block';
-//     }
-// };
